2d44765e05cc678ea6e6de71be153df482ab6d1d
zaid-bashir <zaidwani2282@gmail.com>
2025-01-24 02:15:40 +0400

    first commit

---
+++ src/main/java/com/example/app/FlutterjavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterjavaApplication.class, args);
+	}
+
+}
